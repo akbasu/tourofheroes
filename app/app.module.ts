36f259c19e3fe85ea4da10b54970c52dd85fe8b0
Anindya Basu <akbasu@gmail.com>
2017-01-14 15:31:34 -0600

    New shell AppComponent.

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,8 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroService} from './hero.service';
 
 @NgModule({
   imports:      [ //This array contains the list of external modules used by our application
@@ -12,8 +14,12 @@ import {HeroDetailComponent} from './hero-detail.component';
     ],
   declarations: [ //identifies the application's component. the top of this app's rather bare component tree.
       AppComponent,
-      HeroDetailComponent 
+      HeroDetailComponent,
+      HeroesComponent
     ],
+  providers: [
+    HeroService
+  ],
   bootstrap:    [ AppComponent ] /*identifies this AppComponent as the bootstrap component. When Angular launches the app, 
                                       it places the HTML rendering of AppComponent in the DOM, inside the <my-app>
                                        element tags of the index.html */
