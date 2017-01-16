c0e3fb27bb620ebe012f7d6081e42f38a0a71358
Anindya Basu <akbasu@gmail.com>
2017-01-16 17:56:59 -0600

    Introduced Routing Module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
+
 
 import { AppComponent }  from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
@@ -9,24 +9,13 @@ import {HeroesComponent} from './heroes.component';
 import {DashboardComponent} from './dashboard.component'
 import {HeroService} from './hero.service';
 
+import {AppRoutingModule} from './app-routing.module';
+
 @NgModule({
   imports:      [ //This array contains the list of external modules used by our application
       BrowserModule, 
       FormsModule,
-      RouterModule.forRoot([
-      {
-        path: '', redirectTo: '/dashboard', pathMatch: 'full'
-      },  
-      {
-        path:'heroes', component: HeroesComponent
-      },
-      {
-        path: 'dashboard', component: DashboardComponent
-      },
-      {
-        path: 'hero/:id', component: HeroDetailComponent
-      }
-    ]) 
+      AppRoutingModule      
   ],
   declarations: [ //identifies the application's component. the top of this app's rather bare component tree.
       AppComponent,
